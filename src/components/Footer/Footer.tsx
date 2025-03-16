1d6fb8f7c52b786911819086fa287bdae662316d
gabrielFabro <fabrogabrielandres@gmail.com>
2025-03-16 11:53:46 +0100

    footer texts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import { MaxWidthWrapper } from "../MaxWidthWrapper/MaxWidthWrapper";
+import { useTranslations } from "next-intl";
 
 export const Footer = () => {
+  const t = useTranslations("Components.Footer");
+
   return (
     <footer className="bg-white h-20 relative">
       <MaxWidthWrapper>
@@ -11,7 +14,7 @@ export const Footer = () => {
         <div className="h-full flex flex-col md:flex-row md:justify-between justify-center items-center">
           <div className="text-center md:text-left pb-2 md:pb-0">
             <p className="text-sm text-muted-foreground">
-              &copy; {new Date().getFullYear()} All rights reserved
+              &copy; {new Date().getFullYear()} {t.rich("reserved")}
             </p>
           </div>
 
@@ -21,19 +24,19 @@ export const Footer = () => {
                 href="#"
                 className="text-sm text-muted-foreground hover:text-gray-600"
               >
-                Terms
+                {t.rich("terms")}
               </Link>
               <Link
                 href="#"
                 className="text-sm text-muted-foreground hover:text-gray-600"
               >
-                Privacy Policy
+                {t.rich("privacy")}
               </Link>
               <Link
                 href="#"
                 className="text-sm text-muted-foreground hover:text-gray-600"
               >
-                Cookie Policy
+                {t.rich("cookie")}
               </Link>
             </div>
           </div>
