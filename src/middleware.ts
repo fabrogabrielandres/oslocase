103c228d2ab62407fb37e135b2cfac228c9168b1
gabreil <fabrogabrielandres@gmail.com>
2025-06-08 12:44:08 +0200

    refactor: reorganize middleware setup and improve route exclusion logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,31 @@
+// import createMiddleware from 'next-intl/middleware';
+// import {routing} from './i18n/routing';
+ 
+// export default createMiddleware(routing);
+ 
+// export const config = {
+//   // Match only internationalized pathnames
+//   matcher: ['/', '/(es|en|no)/:path*']
+// };
+
+
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
- 
+import { routing } from './i18n/routing';
+
+const intlMiddleware = createMiddleware(routing);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function middleware(req: any) {
+  const pathname = req.nextUrl.pathname;
+  
+  // Excluye las rutas de Kinde Auth
+  if (pathname.startsWith('/api/auth')) {
+    return;
+  }
+
+  return intlMiddleware(req);
+}
+
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(es|en|no)/:path*']
 };
