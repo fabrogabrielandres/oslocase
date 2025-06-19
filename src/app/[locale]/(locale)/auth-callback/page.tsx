98cc261a1a4e312f77aeade2d3b69de026065021
gabreil <fabrogabrielandres@gmail.com>
2025-06-20 00:35:10 +0200

    fix: update import statements for useRouter in auth-callback and DesignPreview components

--- src/app/[locale]/(locale)/auth-callback/page.tsx
+++ src/app/[locale]/(locale)/auth-callback/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { getAuthStatus } from "./actions";
 import { Loader2 } from "lucide-react";
 import { locales, useRouter } from "@/i18n/routing";
-// import { useRouter } from "next/navigation";
+
 
 const Page = () => {
   const [configId, setConfigId] = useState<string | null>(null);
--- src/app/[locale]/(locale)/configure/preview/DesignPreview.tsx
+++ src/app/[locale]/(locale)/configure/preview/DesignPreview.tsx
@@ -13,7 +13,7 @@ import { LoginModal } from "@/components/LoginModal/LoginModal";
 import { useLocale } from "next-intl";
 import { toast } from "@/hooks/use-toast";
 import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "@/i18n/navigation";
+import { useRouter } from "@/i18n/routing";
 
 interface Props {
   configuration: ConfigurationInterface;
