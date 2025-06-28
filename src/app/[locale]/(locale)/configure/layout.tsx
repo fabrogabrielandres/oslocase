240d5de2290603e6458f21ebf03c5e8916e4d649
gabreil <fabrogabrielandres@gmail.com>
2025-06-28 17:13:10 +0200

    wrap layout component with AuthProvider for user context

--- src/app/[locale]/(locale)/configure/layout.tsx
+++ src/app/[locale]/(locale)/configure/layout.tsx
@@ -2,6 +2,7 @@ import { MaxWidthWrapper } from "@/components/MaxWidthWrapper/MaxWidthWrapper";
 import Steps from "@/components/Steps/Steps";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs";
+import { AuthProvider } from "@/components";
 
 // const Layout = ({ children }: { children: ReactNode }) => {
 //   const { getUser } = getKindeServerSession();
@@ -24,11 +25,15 @@ export default async function Layout({
   const { getUser } = getKindeServerSession();
   const user: KindeUser<Record<string, unknown>> | null = await getUser();
   return (
-    <MaxWidthWrapper className="flex-1 flex flex-col">
-      <div>layout-- {user ? JSON.stringify(user) : "No user logged in layout"}</div>;
-
-      <Steps />
-      {children}
-    </MaxWidthWrapper>
+    <AuthProvider>
+      <MaxWidthWrapper className="flex-1 flex flex-col">
+        <div>
+          layout-- {user ? JSON.stringify(user) : "No user logged in layout"}
+        </div>
+        ;
+        <Steps />
+        {children}
+      </MaxWidthWrapper>
+    </AuthProvider>
   );
 }
