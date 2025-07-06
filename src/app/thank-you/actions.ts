79d71a426105f0ce14fb3f672a67a2dd3a077bb7
gabreil <fabrogabrielandres@gmail.com>
2025-07-07 01:22:25 +0200

    test action server

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -6,12 +6,13 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
+  
 
   console.log("action -> getPaymentStatus", 'User data:', user);
   console.log("action -> getPaymentStatus", 'orderId:', orderId);
   
   if (!user?.id || !user.email) {
-    throw new Error('You need to be logged in to view this page.')
+    throw new Error(`You need to be logged in to view this page. user ${JSON.stringify(user)} `)
   }
 
   // const order = await prisma.order.findFirst({
