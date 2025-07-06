2aeebdf7364b2289ce1e29266a858663e1e5bd65
gabreil <fabrogabrielandres@gmail.com>
2025-07-06 16:44:29 +0200

    feat: simulate order retrieval in getPaymentStatus action and log orderId

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { prisma } from '@/db/prisma'
+// import { prisma } from '@/db/prisma'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
@@ -8,20 +8,23 @@ export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
   const user = await getUser()
 
   console.log("action -> getPaymentStatus", 'User data:', user);
+  console.log("action -> getPaymentStatus", 'orderId:', orderId);
   
   if (!user?.id || !user.email) {
     throw new Error('You need to be logged in to view this page.')
   }
 
-  const order = await prisma.order.findFirst({
-    where: { id: orderId, userId: user.id },
-    include: {
-      billingAddress: true,
-      configuration: true,
-      shippingAddress: true,
-      user: true,
-    },
-  })
+  // const order = await prisma.order.findFirst({
+  //   where: { id: orderId, userId: user.id },
+  //   include: {
+  //     billingAddress: true,
+  //     configuration: true,
+  //     shippingAddress: true,
+  //     user: true,
+  //   },
+  // })
+  const order = {...user, isPaid:true}
+   // Simulating an order for demonstration purposes
   console.log("action -> getPaymentStatus", 'Order data:', order);
   
 
