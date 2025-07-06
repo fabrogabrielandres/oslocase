f02f11ea0fbef989a9d6a3d9414e7c90dedccab8
gabreil <fabrogabrielandres@gmail.com>
2025-07-06 12:11:25 +0200

    feat: update middleware configuration to remove matcher for improved routing flexibility

--- src/middleware.ts
+++ src/middleware.ts
@@ -123,5 +123,5 @@ export default withAuth(async function middleware(req: unknown) {
 });
 
 export const config = {
-  matcher: ["/((?!api/auth|_next|login|logout|[^?]*\\..*).*)"],
+  matcher: [],
 };
