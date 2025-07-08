e3cf4cbe766f8779eddb834b791d46297793058a
gabreil <fabrogabrielandres@gmail.com>
2025-07-08 12:34:45 +0200

    test callback

--- src/components/Provaiders/AuthProviderKinde/AuthProviderKinde.tsx
+++ src/components/Provaiders/AuthProviderKinde/AuthProviderKinde.tsx
@@ -8,8 +8,7 @@ interface Props {
 export const AuthProvider = ({ children }: Props) => {
   return (
     <KindeProvider
-      cookieDomain={process.env.NEXT_PUBLIC_SERVER_URL} // Ej: ".dominio.com"
-      cookieSecure={true}
+    
     >
       {children}
     </KindeProvider>
