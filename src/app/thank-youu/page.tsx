96083d5e6ab2630f1c4daae8fbc1fcef30b1e7d6
gabreil <fabrogabrielandres@gmail.com>
2025-07-08 14:00:26 +0200

    .

--- src/app/thank-youu/page.tsx
+++ src/app/thank-youu/page.tsx
@@ -3,11 +3,14 @@ import ThankYou from "./ThankYou";
 import { AuthTest } from "@/components/testauth/AuthTest";
 import AuthKindeBrosertest from "@/components/testauth/AuthKindeBrosertest";
 
-const Page = () => {
+const Page = ({ searchParams }: { searchParams: { session_id: string } }) => {
+  console.log(searchParams);
+  
   return (
     <Suspense>
       <AuthTest></AuthTest>
       <AuthKindeBrosertest></AuthKindeBrosertest>
+      <div>{JSON.stringify(searchParams)}</div>
       <ThankYou />
     </Suspense>
   );
