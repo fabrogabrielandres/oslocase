60758639fe826de99ef40c3fb812098cb6da6f5f
gabreil <fabrogabrielandres@gmail.com>
2025-07-16 13:40:49 +0200

    fix modal responsibe

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -19,7 +19,8 @@ export const LoginModal = ({
 }) => {
   return (
     <Dialog onOpenChange={setIsOpen} open={isOpen}>
-      <DialogContent className="absolute z-[9999999]">
+      {/* <DialogContent className="fixed z-[9999999] max-h-[85vh] overflow-y-auto sm:max-w-[425px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[90vw]"> */}
+      <DialogContent className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 sm:rounded-lg md:w-full max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <div className="relative mx-auto w-24 h-24 mb-2">
             <Image
