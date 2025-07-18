f7169ab4443a527090a8551030a952d4df651106
gabreil <fabrogabrielandres@gmail.com>
2025-07-18 13:09:01 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,44 +5,10 @@ import Navbar from "@/components/NavBar/NavBar";
 import { AuthProvider, Footer } from "@/components";
 import { Toaster } from "@/components/ui/toaster";
 import ReactQueryProvider from "@/components/Provaiders/QueryClientProvider/QueryClientProvider";
-import { Metadata } from "next";import PaypalProvider from "@/components/Provaiders/PaypalProvider/PaypalProvider";
+import PaypalProvider from "@/components/Provaiders/PaypalProvider/PaypalProvider";
+import { constructMetadata } from "@/lib/utils";
 
-export const metadata: Metadata = {
-  title: "Custom Phone Cases | Design Your Unique Case with Your Photos",
-  description:
-    "Personalize your phone case with your favorite images. Choose materials (plastic, silicone, eco-friendly) and order online. Fast shipping worldwide!",
-  keywords: [
-    "custom phone cases",
-    "personalized phone cover",
-    "design your own case",
-    "photo phone case",
-    "eco-friendly phone cases",
-  ],
-  openGraph: {
-    title: "Custom Phone Cases | Design Your Unique Case with Your Photos",
-    description:
-      "Personalize your phone case with your favorite images. Choose materials and order online. Fast shipping!",
-    url: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-    siteName: "Oslo Case",
-    images: [
-      {
-        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/moose-1.png`, // Replace with your OG image (1200x630px)
-        width: 1200,
-        height: 630,
-        alt: "Custom phone case with personalized photo",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Custom Phone Cases | Design Your Unique Case with Your Photos",
-    description:
-      "Create a one-of-a-kind phone case with your photos. Order today!",
-    images: [`${process.env.NEXT_PUBLIC_SERVER_URL}/moose-1.png`],
-  },
-};
-
+export const metadata = constructMetadata();
 export default async function LocaleLayout({
   children,
 }: {
